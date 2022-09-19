ee32effa7b1c229866fc8f98fe4547204ad138bf
Vidit <z5421284@ad.unsw.edu.au>
2022-09-19 21:05:00 +1000

    Adding auth registerV1 stub

--- src/auth.js
+++ src/auth.js
@@ -8,7 +8,7 @@ function authLoginV1(email, password) {
   
 // Authorises registration given registration details
 // Return User ID on success
-function authRegister(email, password, nameFirst, nameLast) {
+function authRegisterV1(email, password, nameFirst, nameLast) {
     return {
       authUserId: 1,
     }
