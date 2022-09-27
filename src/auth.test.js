d7ef21bd51b1fce3ab78ee1da7cbe73dd0055c11
Jayden Hunter <z5416114@ad.unsw.edu.au>
2022-09-27 20:07:51 +1000

    Fix and implement more tests for AuthLoginV1

--- src/auth.test.js
+++ src/auth.test.js
@@ -3,16 +3,29 @@ import { authLoginV1, authRegisterV1 } from './auth';
 test('Test successful login', () => {
   const user1 = authRegisterV1('johnS@email.com', 'passJohn', 'John', 'Smith');
   const user2 = authRegisterV1('aliceP@fmail.au', 'alice123', 'Alice', 'Person');
-  expect(authLoginV1('johnS@email.com', 'passJohn').authUserId === user1.authUserId);
+  expect(authLoginV1('johnS@email.com', 'passJohn').authUserId).toStrictEqual(user1.authUserId);
   const user3 = authRegisterV1('jamieS@later.co', '&##@P', 'Jamie', 'Son');
-  expect(authLoginV1('aliceP@fmail.au', 'alice123').authUserId === user2.authUserId);
-  expect(authLoginV1('jamieS@later.co', '&##@P').authUserId === user3.authUserId);
-  
+  expect(authLoginV1('aliceP@fmail.au', 'alice123').authUserId).toStrictEqual(user2.authUserId);
+  expect(authLoginV1('jamieS@later.co', '&##@P').authUserId).toStrictEqual(user3.authUserId);
 });
 
-test('Test invalid login', () => {
+test('Test invalid password', () => {
   const user1 = authRegisterV1('johnS@email.com', 'passJohn', 'John', 'Smith');
   const user2 = authRegisterV1('aliceP@fmail.au', 'alice123', 'Alice', 'Person');
   expect(authLoginV1('johnS@email.com', 'wrongpassword')).toStrictEqual({ error: 'Incorrect Password.' });
   expect(authLoginV1('not@person.co', 'abc123')).toStrictEqual({ error: 'Username Not Found.' });
+});
+
+test('Test invalid email', () => {
+  const user1 = authRegisterV1('johnS@email.com', 'passJohn', 'John', 'Smith');
+  const user2 = authRegisterV1('aliceP@fmail.au', 'alice123', 'Alice', 'Person');
+  expect(authLoginV1('not@person.co', 'abc123')).toStrictEqual({ error: 'Username Not Found.' });
+});
+
+test('Test no users login', () => {
+  expect(authLoginV1('not@person.co', 'abc123')).toStrictEqual({ error: 'Username Not Found.' });
+});
+
+test('Test empty string login', () => {
+  expect(authLoginV1('', '')).toStrictEqual({ error: 'Username Not Found.' });
 });
