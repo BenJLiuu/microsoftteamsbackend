6a2642aa58b4239b36fed935326ee64a69f41030
Jayden Hunter <z5416114@ad.unsw.edu.au>
2022-10-03 14:18:04 +1100

    Implement channelsCreateV1 and create error code standard

---
+++ error-codes.md
@@ -0,0 +1,8 @@
+# Error Codes
+Frequently used errors that should appear between similar processes, in order to standardize error output.
+
+Only a suggestion.
+
+## Users
+- Invalid permissions (wrong authUserId): `"Invalid user permissions."`
+- 
--- src/channels.js
+++ src/channels.js
@@ -1,3 +1,6 @@
+import { getData, setData } from './dataStore.js';
+import { validUserId } from './users.js';
+
 //Lists all channels according to authUserId
 function channelsListAllV1 (authUserId) {
   return {
@@ -26,7 +29,23 @@ function channelsListV1(authUserId) {
 // and whether it should be public/private.
 // Returns the new channel id.
 function channelsCreateV1(authUserId, name, isPublic ) {
-    return {
-        channelId: 1,
-    }
+  if (!validUserId(authUserId)) return {
+    error: "Invalid user permissions.",
+  };
+  if (name.length < 1 || name.length > 20) return {
+    error: "Channel name must be between 1-20 characters.",
+  };
+  const data = getData();
+  const newChannelId = new Date().getTime();
+  let channel = {
+    channelId: newChannelId,
+    name: name,
+    isPublic: isPublic,
+    ownerMembers: [authUserId],
+    allMembers: [authUserId],
+  };
+  data.channels.push(channel);
+  setData(data);
 }
+
+export { channelsCreateV1 };
