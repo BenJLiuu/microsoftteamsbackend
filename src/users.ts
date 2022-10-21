3e5388ab90febc24bcc2032a0fcc84807067dffb
Jayden Hunter <z5416114@ad.unsw.edu.au>
2022-10-21 14:58:33 +1100

    Write stub for users/all/v1

--- src/users.ts
+++ src/users.ts
@@ -8,15 +8,9 @@ import { removePassword } from './helper';
   * @param {integer} authUserId - Id of user sending the request to view the profile.
   * @param {integer} uId - Id of user, whose profile is to be viewed.
   *
-  * @returns {error: 'authUserId is invalid.'} - authUserId does not correspond to an existing user.
-  * @returns {error: 'uId does not refer to a valid user.'}  - uId does not correspond to an existing user.
-  * @returns {
-  *   uId: integer,
-  *   nameFirst: string,
-  *   nameLast: string,
-  *   email: string,
-  *   handleStr: string
-  * } - Users verified and user profile is returned.
+  * @returns {Object} {error: 'authUserId is invalid.'} - authUserId does not correspond to an existing user.
+  * @returns {Object} {error: 'uId does not refer to a valid user.'}  - uId does not correspond to an existing user.
+  * @returns {UserOmitPassword} - User profile, without password key.
 */
 export function userProfileV1 (authUserId: number, uId: number): UserOmitPassword | Error {
   const data = getData();
@@ -33,4 +27,18 @@ export function userProfileV1 (authUserId: number, uId: number): UserOmitPasswor
   const privateUser = removePassword(user);
 
   return { user: privateUser };
+}
+
+export function usersAllV1 (token: string): PrivateUser[] | Error {
+  return {
+    users: [
+      {
+        uId: number,
+        nameFirst: string,
+        nameLast: string,
+        email: string,
+        handleStr: string,
+        passwordHash: string,
+      },
+  ]};
 }
