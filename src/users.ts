136b90985fbd7bb3e05294ac87c2c39733baea00
Jayden Hunter <z5416114@ad.unsw.edu.au>
2022-10-22 20:15:59 +1100

    Implement userProfileV2

--- src/users.ts
+++ src/users.ts
@@ -6,24 +6,18 @@ import validator from 'validator';
 /**
   * For a valid user, returns information about a requested valid user profile
   *
-  * @param {integer} authUserId - Id of user sending the request to view the profile.
-  * @param {integer} uId - Id of user, whose profile is to be viewed.
+  * @param {integer} token - Token of user requesting the profile.
+  * @param {integer} uId - Id of user whose profile is to be viewed.
   *
   * @returns {Object} {error: 'authUserId is invalid.'} - authUserId does not correspond to an existing user.
   * @returns {Object} {error: 'uId does not refer to a valid user.'}  - uId does not correspond to an existing user.
   * @returns {UserOmitPassword} User profile, without password key.
 */
-export function userProfileV1 (authUserId: number, uId: number): UserOmitPassword | Error {
+export function userProfileV2 (token: string, uId: number): UserOmitPassword | Error {
+  if (!validToken(token)) return { error: 'Invalid Session Id.' };
+  if (!validUserId(uId)) return { error: 'Invalid User Id.' };
+
   const data = getData();
-
-  if (!(data.users.some(user => user.uId === authUserId))) {
-    return { error: 'authUserId is invalid.' };
-  }
-
-  if (!(data.users.find(user => user.uId === uId))) {
-    return { error: 'uId does not refer to a valid user.' };
-  }
-
   const user = data.users.find(user => user.uId === uId);
   const privateUser = removePassword(user);
 
