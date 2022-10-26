ad2ea4964bd0152de0e43b5aa68b57cc0ff53e45
Vidit <z5421284@ad.unsw.edu.au>
2022-10-27 02:20:16 +1100

    changed handle generation into helper function and added updated handle generation test

--- src/helper.ts
+++ src/helper.ts
@@ -144,3 +144,36 @@ export function checkUserIdtoDm(authUserId : number, dmId : number) : boolean {
   const position = data.dms.findIndex(dm => dm.dmId === dmId);
   return data.dms[position].members.some(user => user.uId === authUserId);
 }
+
+/**
+ * Generates a unique handle for a user
+ *
+ * @param {string} nameFirst - first name of the user
+ * @param {string} nameLast - last name of the user
+ *
+ * @returns {string} handleStr - generated handle for user
+ */
+export function generateHandleStr(nameFirst: string, nameLast: string): string {
+  let handleStr = nameFirst + nameLast;
+  handleStr = handleStr.toLowerCase();
+  handleStr = handleStr.replace(/[^a-z0-9]/gi, '');
+  if (handleStr.length > 20) {
+    handleStr = handleStr.substring(0, 20);
+  }
+  let num = 0;
+  let handleStringExists = false;
+  const data = getData();
+  for (const user of data.users) {
+    if (handleStr === user.handleStr) {
+      num = 0;
+      handleStringExists = true;
+    } else if ((handleStr === user.handleStr.substring(0, handleStr.length)) && /^\d$/.test(user.handleStr[user.handleStr.length - 1])) {
+      num++;
+    }
+  }
+  if (handleStringExists) {
+    handleStr += num;
+  }
+
+  return handleStr;
+}
