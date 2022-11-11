af4d88bfa88588563580208a01a2ab2b9141d383
Vidit <z5421284@ad.unsw.edu.au>
2022-11-11 16:34:08 +1100

    added tests for standupActive

--- src/tests/standup.test.ts
+++ src/tests/standup.test.ts
@@ -15,34 +15,69 @@ describe('standupSendV1 tests', () => {
 
   test('error: invalid token', () => {
     const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
-    const channel = requestChannelsCreate(user.token, 'Example', 'true');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
     expect(requestStandupStart(user.token + 'x', channel.channelId, 1)).toEqual(403);
   });
   test('error: invalid channel', () => {
     const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
-    const channel = requestChannelsCreate(user.token, 'Example', 'true');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
     expect(requestStandupStart(user.token, channel.channelId + 69, 1)).toEqual(400);
   });
   test('error: negative length', () => {
     const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
-    const channel = requestChannelsCreate(user.token, 'Example', 'true');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
     expect(requestStandupStart(user.token, channel.channelId, -3)).toEqual(400);
   });
   test('error: active standup currently running in channel', () => {
     const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
-    const channel = requestChannelsCreate(user.token, 'Example', 'true');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
     requestStandupStart(user.token, channel.channelId, 1);
     expect(requestStandupStart(user.token, channel.channelId, 1)).toEqual(400);
   });
   test('error: channelId is valid but authorised user is not a member of the channel', () => {
     const user1 = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
-    const user2 = requestAuthRegister('creed@gmail.com', 'creed123', 'Creed', 'Bratton')
-    const channel = requestChannelsCreate(user.token, 'Example', 'true');
+    const user2 = requestAuthRegister('creed@gmail.com', 'creed123', 'Creed', 'Bratton');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
     expect(requestStandupStart(user2.token, channel.channelId, 1)).toEqual(403);
   });
   test('successful start', () => {
     const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
-    const channel = requestChannelsCreate(user.token, 'Example', 'true');
-    expect(requestStandupStart(user.token, channel.channelId, 1)).toStrictEqual(Number); // possibly change
+    const channel = requestChannelsCreate(user.token, 'Example', true);
+    expect(requestStandupStart(user.token, channel.channelId, 1)).toStrictEqual({ timeFinish: Number }); // possibly change
+  });
+});
+
+describe('standupActiveV1 tests', () => {
+  beforeEach(() => {
+    requestClear();
+  });
+
+  test('error: invalid token', () => {
+    const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
+    expect(requestStandupActive(user.token + 'x', channel.channelId)).toEqual(403);
+  });    
+  test('error: invalid channel', () => {
+    const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
+    expect(requestStandupActive(user.token, channel.channelId + 69)).toEqual(400);
+  });
+  test('error: channelId is valid but authorised user is not a member of the channel', () => {
+    const user1 = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
+    const user2 = requestAuthRegister('creed@gmail.com', 'creed123', 'Creed', 'Bratton');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
+    expect(requestStandupActive(user2.token, channel.channelId)).toEqual(403);
+  });
+  test('success: no standup is active', () => {
+    const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
+    expect(requestStandupActive(user.token, channel.channelId)).toStrictEqual({ isActive: false, timeFinish: null });
+  });
+  test('success: standup is active', () => {
+    const user = requestAuthRegister('kevin@gmail.com', 'office123', 'Kevin', 'Malone');
+    const channel = requestChannelsCreate(user.token, 'Example', true);
+    const timeNow = (Math.floor((new Date()).getTime() / 1000));
+    requestStandupStart(user.token, channel.channelId, 1);
+    expect(requestStandupActive(user.token, channel.channelId)).toStrictEqual({ isActive: false, timeFinish: timeNow + 1 });
   });
 });
