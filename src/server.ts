5455ed53facd1f20ecdc376670116d539a5145e5
Jayden Hunter <z5416114@ad.unsw.edu.au>
2022-11-15 20:52:08 +1100

    Fix linting

--- src/server.ts
+++ src/server.ts
@@ -9,16 +9,16 @@ import { authRegisterV3, authLoginV3, authLogoutV2 } from './auth';
 import { channelDetailsV3, channelJoinV3, channelInviteV3, channelMessagesV3, channelLeaveV2, channelRemoveOwnerV2, channelAddOwnerV2 } from './channel';
 import { echo } from './echo';
 import { clearV1 } from './other';
-<<<<<<< HEAD
 import { usersAllV2 } from './users';
 import { userProfileSetNameV2, userProfileSetEmailV2, userProfileSetHandleV2, userProfileV3, userStatsV1 } from './user';
 import { dmCreateV2, dmListV2, dmLeaveV2, dmMessagesV2, dmDetailsV2, dmRemoveV2 } from './dm';
-import { messageSendDmV2, messageSendV2, messageEditV2, messageRemoveV2, messageSendlaterV1, messageSendlaterDmV1, searchV1, notificationsGetV1 } from './message';
-=======
-import { userProfileV3, usersAllV2, userProfileSetNameV2, userProfileSetEmailV2, userProfileSetHandleV2, notificationsGetV1 } from './users';
-import { dmCreateV2, dmListV2, ddmLeaveV2, dmMessagesV2, dmDetailsV2, dmRemoveV2 } from './dm';
-import { messageSendDmV2, messageSendV2, messageEditV2, messageRemoveV2, messageShareV1, messageReactV1, messageUnreactV1, messagePinV1, messageUnpinV1, messageSendlaterV1, messageSendlaterDmV1, searchV1 } from './message';
->>>>>>> c3e7d6c813ec00ba9141b4e316b808ac2dede20a
+import {
+  notificationsGetV1, messageSendDmV2, messageSendV2,
+  messageEditV2, messageRemoveV2, messageShareV1,
+  messageReactV1, messageUnreactV1, messagePinV1,
+  messageUnpinV1, messageSendlaterV1, messageSendlaterDmV1,
+  searchV1
+} from './message';
 
 // Set up web app
 const app = express();
